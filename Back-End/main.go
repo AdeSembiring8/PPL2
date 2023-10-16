1cad6bd7f6995dccd0cd2c199023b9f49d608e2d
irka021002 <irfankamal021002@gmail.com>
2023-10-16 13:31:59 +0700

    authentication

---
+++ Back-End/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+type User struct {
+    Username string
+    Password string
+}
+
+func Register(username, password string) error {
+    collection := client.Database("yourDBName").Collection("users")
+    user := User{Username: username, Password: password}
+
+    _, err := collection.InsertOne(context.Background(), user)
+    return err
+}
+
+func Login(username, password string) bool {
+    collection := client.Database("yourDBName").Collection("users")
+    var user User
+    err := collection.FindOne(context.Background(), User{Username: username, Password: password}).Decode(&user)
+
+    if err != nil {
+        return false
+    }
+
+    return true
+}
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+    // Handle user registration here
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+    // Handle user login here
+}
+
+func main() {
+    ConnectDB()
+    r := mux.NewRouter()
+    r.HandleFunc("/register", RegisterHandler).Methods("POST")
+    r.HandleFunc("/login", LoginHandler).Methods("POST")
+
+    http.Handle("/", r)
+    http.ListenAndServe(":8080", nil)
+}
