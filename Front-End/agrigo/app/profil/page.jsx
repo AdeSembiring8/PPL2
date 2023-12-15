1ea385bc598ed9d8f08cf37351a7e0e3c1bb2f0d
AdeSembiring8 <fatwasembiring8@gmail.com>
2023-12-15 12:36:50 +0700

    Add Addres at Profile Page

--- Front-End/agrigo/app/profil/page.jsx
+++ Front-End/agrigo/app/profil/page.jsx
@@ -6,15 +6,22 @@ import { redirect, useRouter } from "next/navigation";
 export default function Home() {
     const [username, setUsername] = useState('');
     const router = useRouter();
+    const [alamat, setAlamat] = useState('');
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
             const username = localStorage.getItem('name');
             setUsername(username);
+
+            const nilaiDariLocalStorage = localStorage.getItem('alamat');
+            setAlamat(nilaiDariLocalStorage);
+            console.log('Data', alamat)
         }
+
+
     }, []);
 
-    const handleLogout = async  (e) => {
+    const handleLogout = async (e) => {
         try {
             const response = await fetch('http://52.221.249.20:8080/api/logout', {
                 method: 'POST', // Atau metode HTTP yang sesuai
@@ -60,42 +67,59 @@ export default function Home() {
                 </div>
             </nav>
 
-            <main className=' bg-white h-screen'>
+            <main className=' bg-white h-full self-center flex flex-col justify-center items-center'>
                 <div className=" pt-16 px-48 mx-48 pb-10">
                     <h1 className=" text-black text-4xl font-bold">
-                        Edit Profil
+                        Profil
                     </h1>
                 </div>
 
-                <div className='flex pt-10'>
-                    <div className=''>
-                        <div className=' px-48 ml-48' >
+                <div className=' flex self-center pt-10  flex-col justify-center items-center'>
+                    <div className='flex-col justify-center items-center'>
+                        <div className=' flex flex-col justify-center items-center' >
                             <h1 className='text-black text-xl font-bold' > Avatar Profil</h1>
                         </div>
-                        <div className="block rounded-lg bg-custom-F2F2F2 p-6 ml-96 mt-4 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] w-fit ">
-                            <div className="">
-                                <div className=" self-center">
-                                    {username && <img className="p-2 aspect-w-1 self-center aspect-h-1 w-80 h-80" href="#" alt='avatar' src={`http://52.221.249.20:8080/api/generateAvatar?name=${username}`}></img>}
-                                </div>
-                                <div className=' '>
-                                    <button
-                                        className="text-white bg-custom-C2AF00 w-80 h-10 rounded-lg my-5 font-bold self-center">
-                                        Foto Avatar
-                                    </button>
-                                </div>
-                                <div>
-                                    <button onClick={handleLogout}
-                                    className=" text-red-600 bg-white border-red-600 border-2 w-80 h-10 rounded-lg font-bold">
-                                        Logout
-                                    </button>
-                                </div>
+                        <div className=' flex flex-col justify-center items-center'>
+
+                        </div>
+                        <div className='flex flex-col justify-center items-center rounded-lg bg-custom-F2F2F2 p-6 mt-4 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]'>
+                            <div className="self-center">
+                                {username && (
+                                    <img className="p-2 aspect-w-1 self-center aspect-h-1 w-80 h-80" href="#" alt='avatar' src={`http://52.221.249.20:8080/api/generateAvatar?name=${username}`}></img>
+                                )}
+                            </div>
+                            <div>
+                                <button onClick={handleLogout} className="text-red-600 bg-white border-red-600 border-2 w-80 h-10 rounded-lg font-bold">
+                                    Logout
+                                </button>
                             </div>
                         </div>
+                        <div className='flex flex-col justify-center text-center  mb-8 w-full items-center self-center'>
+                            <div className=' w-96'>
+                                <p className=" text-black text-xl font-semibold pt-12 pb-5">Alamat Pengiriman</p>
+                            </div>
+                            <div className="rounded-lg text-black p-4 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-custom-F2F2F2 w-fit ">
+                                <p
+                                    className="text-base text-custom-7C7C7C  ">
+                                    {alamat}
+                                </p>
+                            </div>
+
+                        </div>
                     </div>
-                    <div className=' ml-40 pr-48 mr-48 w-full'>
+
+
+                    <div className='  pr-48 mr-48 w-full'>
                     </div>
                 </div>
+
+
             </main>
+            <footer className="bg-gray-200 p-4 text-center">
+                <p className="text-sm text-gray-600">
+                    AgriGo © 2023. All rights reserved.
+                </p>
+            </footer>
         </>
     )
 }
